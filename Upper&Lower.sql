5b1a1109a529408dfde148281cfffba4365ab999
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2026-08-18 16:30:04 -0400

    Add SQL examples for string case conversion
    Added examples of LOWER() and UPPER() functions in SQL.

--- Upper&Lower.sql
+++ Upper&Lower.sql
@@ -1,1 +1,11 @@
+-- LOWER() - Convert a string to lowercase
 
+SELECT *
+FROM employees 
+WHERE LOWER(firstname) = 'leslie'
+
+-- UPPER() - Convert a string to uppercase
+  
+SELECT *
+FROM employees 
+WHERE UPPER(email) = 'DMURPHY@CLASSICMODEL.COM'
