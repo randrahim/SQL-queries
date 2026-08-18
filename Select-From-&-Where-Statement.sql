fbb6f5d91d2279c5602d61a36221ea6e6c81f780
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2026-08-18 16:18:30 -0400

    Add SQL queries for customer data retrieval

---
+++ Select-From-&-Where-Statement.sql
@@ -0,0 +1,16 @@
+/* 1) all Records where customers last name is not Young: */
+
+SELECT * FROM classicmodels.customers
+WHERE contactlastname <> 'Young'
+
+/* 2) show Customername, Girst Name, Last name, Phone, City & Country where First name is Julie and she is from USA: */
+
+SELECT customername, contactfirstname, contactlastname, phone, city, country
+FROM classmodels.customers
+WHERE country = 'USA' and contactfirstname = 'Julie'
+
+/* 3) First Name and Last name for customers from Norway or Sweden: */
+
+SELECT contactfirstname, contactlastname 
+FROM classicmodel.customers
+WHERE country = 'Norway' or country = 'Sweden'
