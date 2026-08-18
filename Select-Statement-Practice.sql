2526d87de12a7266f3ece50996e53d4e9233edb0
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2026-08-18 16:26:15 -0400

    Add SQL queries for customers and sales reps

--- Select-Statement-Practice.sql
+++ Select-Statement-Practice.sql
@@ -1,3 +1,11 @@
 -- Show all columns for customers from the USA with surname Brown:
 
+SELECT * 
+FROM customers 
+WHERE country = 'USA'
+AND contactlastName = 'Brown'
+
 -- For employees who are sales reps display their email:
+SELECT email
+FROM employees 
+WHERE jobTitle = 'Sales Rep'
