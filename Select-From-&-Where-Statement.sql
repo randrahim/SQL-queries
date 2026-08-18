cd2ddf3d5eb2460a7b7cc35df48bfbc4d72deca9
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2026-08-18 16:23:09 -0400

    Comment out SQL queries in Select-From-Where statement
    Commented out SQL queries for customer selection based on conditions.

--- Select-From-&-Where-Statement.sql
+++ Select-From-&-Where-Statement.sql
@@ -3,13 +3,13 @@
 SELECT * FROM classicmodels.customers
 WHERE contactlastname <> 'Young'
 
-/* 2) show Customername, Girst Name, Last name, Phone, City & Country where First name is Julie and she is from USA: */
+-- 2) show Customername, Girst Name, Last name, Phone, City & Country where First name is Julie and she is from USA:
 
 SELECT customername, contactfirstname, contactlastname, phone, city, country
 FROM classmodels.customers
 WHERE country = 'USA' and contactfirstname = 'Julie'
 
-/* 3) First Name and Last name for customers from Norway or Sweden: */
+-- 3) First Name and Last name for customers from Norway or Sweden:
 
 SELECT contactfirstname, contactlastname 
 FROM classicmodel.customers
