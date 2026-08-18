c3060773273f560bc8e0b88187e993780ea5e065
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2026-08-18 16:40:54 -0400

    Add SQL query for customers in New York, London, Mumbai
    Added query to select customers from specific cities.

--- In-&-Not-In.sql
+++ In-&-Not-In.sql
@@ -15,3 +15,9 @@ WHERE UPPER(email) NOT IN (
   'pmarsh@classicmodelcars.com',
   'gbondur@classicmodelcars.com',
   'abow@classicmodelcars.com')
+
+-- Show all columns for customers who are from New York, London or Mumbai using IN
+  
+SELECT *
+FROM customer
+WHERE cust_city IN ('New York', 'London', 'Mumbai')
