ffe2998f926deeb1e9e2349128bc8a5a4892eeb9
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2026-08-18 16:46:19 -0400

    Add SQL queries for distinct countries and ordering

--- Distinct-Like-&-Order-By.sql
+++ Distinct-Like-&-Order-By.sql
@@ -1,1 +1,19 @@
+-- Select distinct countries from customers table where city has 'New' string:
+
+SELECT DISTINCT country
+FROM customers
+WHERE city
+LIKE '%New%'
+
+-- Select distinct countries from customers table where email address has 'classicmodel' string:
+
+SELECT DISTINCT country
+FROM customers
+WHERE email
+LIKE '%classicmodel%'
+
+-- Select all records and order them by lastname
+SELECT *
+FROM employees
+ORDER BY lastName
 
