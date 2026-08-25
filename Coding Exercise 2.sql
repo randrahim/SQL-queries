518b31c1937487b42a7d164f24a6200ae73b25e2
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2026-08-24 21:36:31 -0400

    Add LEFT JOIN query for customers and orders
    Added SQL query to show all customers and their orders, including those with no orders.

--- Coding Exercise 2.sql
+++ Coding Exercise 2.sql
@@ -1,1 +1,6 @@
+-- Show all customers and their orders, even if they made no orders. Both tables have a cust_code field
 
+SELECT *
+FROM customer cust
+LEFT JOIN orders ord
+ON cust.cust_code = ord.cust_code
