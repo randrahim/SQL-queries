09f552624eb00a7693fc8e81aa292cef17ca6990
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2026-08-24 21:53:38 -0400

    Modify SQL queries for ordering and adding HAVING clause

--- Sum, Round, Group by & Having.sql
+++ Sum, Round, Group by & Having.sql
@@ -1,8 +1,15 @@
 SELECT paymentDate, sum(amount) as total_payments
 FROM payments
 GROUP BY paymentDate
-ORDER BY 1
+ORDER BY total_payments;
 
 SELECT paymentDate, ROUND(SUM(amount),1) as total_payments
 FROM payments
 GROUP BY paymentDate
+ORDER BY 1;
+
+SELECT paymentDate, sum(amount) as total_payments
+FROM payments
+GROUP BY paymentDate
+HAVING total_payments > 5000
+ORDER BY total_payments desc;
