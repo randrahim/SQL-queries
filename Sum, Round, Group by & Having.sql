b7e17f6266c2111a624564f44b3f91dcd5b25b93
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2026-08-24 21:51:37 -0400

    Add SQL queries for summing and rounding payments

--- Sum, Round, Group by & Having.sql
+++ Sum, Round, Group by & Having.sql
@@ -1,1 +1,8 @@
+SELECT paymentDate, sum(amount) as total_payments
+FROM payments
+GROUP BY paymentDate
+ORDER BY 1
 
+SELECT paymentDate, ROUND(SUM(amount),1) as total_payments
+FROM payments
+GROUP BY paymentDate
