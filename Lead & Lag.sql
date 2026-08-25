8baa942f804ecfd8d1aa95c3e2a1eee2f31b2bf0
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2026-08-25 12:00:41 -0400

    Update Lead & Lag.sql with CTE for LAG example
    Refactor SQL to use common table expression for LAG example.

--- Lead & Lag.sql
+++ Lead & Lag.sql
@@ -19,4 +19,22 @@ SELECT
     partition BY customernumber 
     ORDER BY paymentdate
   ) AS previous_payment
-FROM classicmodels.payments
+FROM payments
+
+-- LAG Example 2
+WITH cte_main AS (
+  SELECT 
+    customernumber,
+    paymentdate,
+    amount,
+    LAG(amount) over (
+      partition BY customernumber 
+      ORDER BY paymentdate
+    ) AS pre_payment
+FROM payments)
+
+SELECT *,
+      amount - pre_payment as difference 
+FROM cte_main
+
+
