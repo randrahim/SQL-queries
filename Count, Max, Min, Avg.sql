b8fbc27bb4e45a87efd8c8e5c6b174b36c3634f3
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2026-08-25 10:52:55 -0400

    Add SQL queries for counting and payment statistics

--- Count, Max, Min, Avg.sql
+++ Count, Max, Min, Avg.sql
@@ -1,1 +1,21 @@
+SELECT COUNT(orderNumber) as Orders
+FROM orders;
 
+SELECT productCode, COUNT(orderNumber) as Orders
+FROM orderdetails
+GROUP BY productCode;
+
+SELECT 
+  paymentDate,
+  MAX(amount) as highest_payment,
+  MIN(amount) as lowest_payment,
+FROM payments
+GROUP BY paymentDate
+HAVING paymentDate = '2003-12-09';
+
+SELECT 
+  paymentDate,
+  AVG(amount) as average_payment_received,
+FROM payments
+GROUP BY paymentDate
+ORDER BY paymentDate;
