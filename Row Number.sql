313f2d33a71f912368b75aacb47ad3555e713e1c
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2026-08-25 11:52:35 -0400

    Refactor SQL queries for row numbering

--- Row Number.sql
+++ Row Number.sql
@@ -1,10 +1,12 @@
 -- Example 1
 SELECT 
-customernumber,
-t1.ordernumber,
-row_number() over (partition BY customernumber order 
-BY orderdate) AS purchase_number
-FROM classicmodels.orders t1
+  customernumber,
+  t1.ordernumber,
+  row_number() over (
+    partition BY customernumber 
+    order BY orderdate) 
+  AS purchase_number
+FROM orders t1
 ORDER BY customernumber, t1.ordernumber; 
 
 
@@ -15,10 +17,13 @@ SELECT DISTINCT
   t1.ordernumber, 
   orderdate,
   productcode,
-  row_number() over (partition BY t3.customernumber ORDER BY orderdate) AS purchase_number
-FROM classicmodels.orders t1
-JOIN classicmodels.orderdetails t2 ON t1.ordernumber = t2.ordernumber
-JOIN classicmodels.customers t3 ON t1.customernumber = t3.customernumber
+  row_number() over 
+    (partition BY t3.customernumber 
+    ORDER BY orderdate) 
+  AS purchase_number
+FROM orders t1
+JOIN orderdetails t2 ON t1.ordernumber = t2.ordernumber
+JOIN customers t3 ON t1.customernumber = t3.customernumber
 ORDER BY t3.customername;
 
 -- Example 3
@@ -26,10 +31,14 @@ WITH main_cte AS (
   SELECT 
     customernumber,
     t1.ordernumber,
-    row_number() over (PARTITION BY customernumber ORDER BY 
-    orderdate) as purchase_number
-  FROM classicmodels.orders t1
+    row_number() over 
+      (PARTITION BY customernumber 
+       ORDER BY orderdate) 
+    AS purchase_number
+  FROM orders t1
   ORDER BY customernumber, t1.ordernumber
 )
-SELECT * FROM main_cte WHERE purchase_number = 2;
+SELECT * 
+FROM main_cte 
+WHERE purchase_number = 2;
 
